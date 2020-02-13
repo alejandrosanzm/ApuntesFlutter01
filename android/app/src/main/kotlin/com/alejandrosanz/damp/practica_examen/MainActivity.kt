6a73e6eee25981a58bae639e6c5aaf7488dca1e3
alejandrosanzzm <asm.driveutad@gmail.com>
2020-02-13 17:58:44 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/alejandrosanz/damp/practica_examen/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.alejandrosanz.damp.practica_examen
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
